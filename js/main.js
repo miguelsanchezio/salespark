c72017c7b479731fd912e9d8b7b24ab9259227e3
Miguel Sanchez <miguelsanchez@Miguels-MacBook-Pro.local>
2019-05-21 19:10:30 -0700

    Added extra functionality using JS.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,29 @@ function initMap() {
     zoom: 14,
     center: loc
   });
-  
+
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+$('#navbar a, .btn').on('click', function(e) {
+  if (this.hash !== '') {
+    // e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      500
+    );
+  }
+});
+
+window.addEventListener('scroll', function() {
+  if(window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
